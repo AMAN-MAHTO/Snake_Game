89e0bce0f470d471d07a4435d6565de97b6170c5
Aman Mahto <amahto@gmail.com>
2021-01-16 12:47:42 +0530

    Intial commit

---
+++ 02.py
@@ -0,0 +1,5 @@
+#calculate simple interest
+principal = int(input("Enter Principal: "))
+years = int(input("Enter no. of years: "))
+rate = int(input("Enter rate: "))
+print("Interest: ",(principal*years*rate) / 100)
---
+++ 03.py
@@ -0,0 +1,7 @@
+#even or odd
+
+number = int(input("Enter number: "))
+if number%2==0:
+    print(number,"is an even number")
+else:
+    print(number,"is an odd number")
